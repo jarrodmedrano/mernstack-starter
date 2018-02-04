83aff5d166d9ab3f3114bceb7f134eddb748d5c9
jmedran <jmedran@gmail.com>
2018-02-03 21:31:23 -0700

    change client

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 if(process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   //like our main.js file
-  app.use(express.static('../client/build'))
+  app.use(express.static('client/build'))
   // Express will serve up index.html file
   // if it does not recognize the route
   const path = require('path');
